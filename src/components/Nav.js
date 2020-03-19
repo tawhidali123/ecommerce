162ce5fa7ac22eed9c2495395c880bfce1775bea
Tawhid <gem@tawhid.fios-router.home>
2020-03-18 22:41:12 -0400

    created style for nav

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavDiv } from '../style/nav'
+
+export default function Nav() {
+    return (
+        <NavDiv>
+            <div className='container'>
+
+                <div className='wrapper'>
+                    <ul>
+                        <li>first item</li>
+                        <li>second item</li>
+                        <li>third item</li>
+                    </ul>
+                </div>
+                
+            </div>
+        </NavDiv>
+    )
+}
