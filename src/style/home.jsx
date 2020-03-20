0c81b50fc196078cfb51c7d758e459c8c089820a
Tawhid <gem@tawhid.fios-router.home>
2020-03-19 23:48:04 -0400

    slider needs completion

---
+++ src/style/home.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const HomeStyled = styled.div`
+    .container{
+        text-align: center;
+
+
+        .slide_container{
+            width: 100%;
+            height: 100%;
+            margin: 50px 0;
+            display: flex;
+            overFlow-x: auto;
+            
+            
+            ${'' /* overFlow: hidden; */}
+
+            button{
+                width: 45px;
+                height: 45px;
+            }
+
+
+            img{
+                width: 75vw;
+                height: 50vh;
+                margin: 30px;
+                margin-top: 50px;
+            }
+        }
+
+
+    }
+
+`;
