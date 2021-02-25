7ffd1999e216244525221bea6416587d0db70eaa
Tawhid <gem@tawhid.fios-router.home>
2021-02-24 20:05:57 -0500

    checkout

--- src/Checkout/CheckoutList.js
+++ src/Checkout/CheckoutList.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import { Context } from '../AppProvider'
+import CheckoutItem from './CheckoutItem'
 
 export default function CheckoutList() {
+
+    let totalDollars = 0
+
     return (
         <Context.Consumer>
             {
                 ({cart}) => (
                     <div>
-                        <div>
+                        <div style={{textAlign: 'center'}}>
                             <h1>Cart Checkout</h1>
                         </div>
                         <div>
-                            {Object.keys(cart).map(cartItem => (
-                                    <div>
-                                        <h2>{cart[cartItem].product.name}</h2>
-                                        <p>Each: ${cart[cartItem].product.price}</p>
-                                        <small>qty: {cart[cartItem].quantity}</small>
-                                        <h4>Total: ${cart[cartItem].product.price * cart[cartItem].quantity}</h4>
-                                    </div>
-                                    
+                            {Object.keys(cart).map(cartItem => {
+                                totalDollars += (cart[cartItem].product.price * cart[cartItem].quantity)
+                            
+                                return(
+                                    <CheckoutItem 
+                                    name={cart[cartItem].product.name} 
+                                    price={cart[cartItem].product.price} 
+                                    quantity={cart[cartItem].quantity}
+                                    />
                                 )
-                            )}
+                                    
+                            })}
+                        </div>
+                        <div style={{float: 'right', marginRight: '10%'}}>
+                            <h1>Total: ${totalDollars}</h1>
                         </div>
                     </div>
                 )
